33b504b066da47e82059fdf59d1d9153141ccb4a
1-2x <xlzspe@gmail.com>
2025-04-01 17:14:58 +0100

    Delete settings.js

--- settings.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll("#settings input:not([disabled])").forEach(input => {
-    input.addEventListener("input", (e) => {
-        if (e.target.value.length > 1) e.target.value = e.target.value[0];
-    });
-});
